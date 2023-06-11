253210ba3cd0f3c81b8487fc16b0105612e61318
DanieII <danielbogdanov1748@gmail.com>
2023-06-11 23:05:35 +0300

    Added most of the logic with the weather api, css and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
 import "./App.css";
-import Seatch from "./components/search";
+import Search from "./components/search";
+import Content from "./components/content";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [city, changeCity] = useState();
+  const [weatherData, changeWeatherData] = useState();
+  const [errorMessage, changeErrorMessage] = useState();
+
+  const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+
+  const onSearch = (currentCity) => {
+    changeCity(currentCity);
+  }
+
+  const getCoordinatesFromCityName = async () => {
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`);
+    const data = await response.json();
+    if (data.length > 0) {
+      const [lat, lon] = [data[0]['lat'], data[0]['lon']];
+      return [lat, lon];
+    } else {
+      changeErrorMessage(`${city} is not a valid city`);
+    }
+  }
+
+  const getWeatherData = async () => {
+    const result = await getCoordinatesFromCityName();
+    if (result) {
+      const [lat, lon] = result;
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`);
+      const data = await response.json();;
+      return { city: city, weather: data["weather"][0]["main"], temp: data["main"]["temp"], feels_like: data["main"]["feels_like"] };
+    }
+  }
+
+  useEffect(() => {
+    if (city) {
+      async function setNewWeatherData() {
+        const newWeatherData = await getWeatherData();
+        if (newWeatherData) {
+          changeWeatherData(newWeatherData);
+        } else {
+          changeWeatherData(null)
+        }
+      }
+      setNewWeatherData();
+    }
+  }, [city])
+
   return (
     <div className="App">
-      <Seatch />
+      {errorMessage && <div className="error"> {errorMessage} </div>}
+      <Search onSearch={onSearch} />
+      <Content weatherData={weatherData} />
     </div>
   );
 }
--- src/components/search.js
+++ src/components/search.js
@@ -1,8 +1,10 @@
 import { useState, useRef, useEffect } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 const LOCAL_STORAGE_KEY = 'lastCity';
 
-const Seatch = ({ getInformation }) => {
+const Search = ({ onSearch }) => {
     const [search, ChangeSearch] = useState();
     const searchRef = useRef();
 
@@ -18,11 +20,18 @@ const Seatch = ({ getInformation }) => {
         ChangeSearch(searchRef.current.value);
     }
 
+    const startSearch = () => {
+        onSearch(searchRef.current.value);
+    }
+
     return (
         <>
-            <input type="text" ref={searchRef} placeholder="City" onChange={handleChange}></input>;
+            <div id="search-box">
+                <input type="text" ref={searchRef} placeholder="Search..." onChange={handleChange}></input>
+                <FontAwesomeIcon icon={faMagnifyingGlass} id="search-icon" onClick={startSearch} />
+            </div>
         </>
     );
 };
 
-export default Seatch;
+export default Search;
